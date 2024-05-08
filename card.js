758318284c7b9ba62980bccfeebc677bea8d4ad9
Kassidi Poe <kassidipoe@gmail.com>
2024-05-08 05:24:01 -0400

    changes

--- card.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-	const card = document.getElementById("card");
-	const outside = document.getElementById("outside");
-  
-	card.addEventListener("click", function () {
-	  outside.classList.toggle("open");
-	});
-  });
-  
